367acad23b8eacd4bf2d85ba5e93ad6c85f26c85
angles <M8R-w53ol1@mailinator.com>
2012-06-15 01:46:20 -0400

    Ctrl-Cmd-s opens selected PathFinder file(s) in Sublime Text 2. Currently, must hard code shortcuts since PathFinder 6 forgets preferences user sets for 3rd party plugins.

--- sublime/OpenInSublime.m
+++ sublime/OpenInSublime.m
@@ -30,9 +30,10 @@
 {
   NSMenuItem* menuItem;
 	
-  menuItem = [[[NSMenuItem alloc] initWithTitle:@"Open in Sublime" action:@selector(pluginAction:) keyEquivalent:@""] autorelease];
+  // must hard code since PathFinder 6 forgets preferences user sets for 3rd party plugins 
+  menuItem = [[[NSMenuItem alloc] initWithTitle:@"Open in Sublime" action:@selector(pluginAction:) keyEquivalent:@"s"] autorelease];
   [menuItem setTarget:self];
-  //[menuItem setKeyEquivalentModifierMask: NSControlKeyMask | NSCommandKeyMask];
+  [menuItem setKeyEquivalentModifierMask: NSControlKeyMask | NSCommandKeyMask];
   return menuItem;
 }
 
